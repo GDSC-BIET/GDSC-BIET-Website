1da8c8b0e49f85e80c50ff226c2971b683300b28
Sai kamal <65682028+Dantusaikamal@users.noreply.github.com>
2021-09-08 17:49:41 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home/Home";
+import Event from "./pages/Events/Event";
+import Projects from "./pages/Projects/Projects";
+import Team from "./pages/Team/Team";
+import Contact from "./pages/Contact/Contact";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// App component
+
+export default function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/reactjs-dscBiet" exact component={Home} />
+          <Route path="/event" exact component={Event} />
+          <Route path="/projects" exact component={Projects} />
+          <Route path="/team" exact component={Team} />
+          <Route path="/contact" exact component={Contact} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
---
+++ src/pages/Home/data/Data.jsx
@@ -0,0 +1,41 @@
+import img1 from "../../../assets/images/Startup-2.svg";
+import img2 from "../../../assets/images/Feedback.svg";
+import img3 from "../../../assets/images/Startup-1.svg";
+import img4 from "../../../assets/images/Project-team.svg";
+
+export const homeObjOne = {
+  headline: "Developers Student Clubs BIET",
+  description:
+    "Developer Student Clubs are university based community groups for students interested in Google developer technologies. Students from all undergraduate or graduate programs with an interest in growing as a developer are welcome.",
+  buttonLabel: "JOIN DSC BIET",
+  imgStart: "",
+  img: img1,
+  alt: "Credit Card",
+};
+
+export const homeObjTwo = {
+  headline: "Connect With DSC BIET",
+  description:
+    "Meet other students on campus interested in similar projects/ technologies. All are welcome, including those with diverse technical backgrounds and different branches.",
+  imgStart: "start",
+  img: img2,
+  alt: "Vault",
+};
+
+export const homeObjThree = {
+  headline: "Why Join DSC BIET",
+  description:
+    "Learn about a wide range of technical topics where new skills are gained through hands-on workshops, in-person training and project building activities.",
+  imgStart: "",
+  img: img3,
+  alt: "Vault",
+};
+
+export const homeObjFour = {
+  headline: "Grow With DSC BIET",
+  description:
+    "Apply your new learnings and connections to build great solutions for local problems. Advance your skills, career and network. Give back to your community by helping others learn as well.",
+  imgStart: "start",
+  img: img4,
+  alt: "Vault",
+};
