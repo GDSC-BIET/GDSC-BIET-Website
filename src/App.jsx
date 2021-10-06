7da6bef64ce438b3e67e46ebc7f09e81aa6bb541
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-10-07 00:34:23 +0530

    fixed navigation1

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,33 @@ import Event from "./pages/Events/Event";
 import Projects from "./pages/Projects/Projects";
 import Team from "./pages/Team/Team";
 import Contact from "./pages/Contact/Contact";
-import { BrowserRouter as Switch, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 // App component
 
 export default function App() {
   return (
     <>
-      <Navbar />
-      <Switch>
-        <Route path="/GDSC-BIET-Website/" exact component={Home} />
-        <Route path="/GDSC-BIET-Website/event" exact component={Event} />
-        <Route path="/GDSC-BIET-Website/projects" exact component={Projects} />
-        <Route path="/GDSC-BIET-Website/team" exact component={Team} />
-        <Route path="/GDSC-BIET-Website/contact" exact component={Contact} />
-        <Redirect to="/GDSC-BIET-Website" />
-      </Switch>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/GDSC-BIET-Website" exact component={Home} />
+          <Route path="/GDSC-BIET-Website/event" exact component={Event} />
+          <Route
+            path="/GDSC-BIET-Website/projects"
+            exact
+            component={Projects}
+          />
+          <Route path="/GDSC-BIET-Website/team" exact component={Team} />
+          <Route path="/GDSC-BIET-Website/contact" exact component={Contact} />
+          <Redirect to="/GDSC-BIET-Website/" />
+        </Switch>
+      </Router>
     </>
   );
 }
