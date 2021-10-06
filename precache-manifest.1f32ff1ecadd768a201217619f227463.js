f84f3069275859cdda3698c30056bf7581560dda
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-10-06 23:12:11 +0530

    Updates

---
+++ precache-manifest.1f32ff1ecadd768a201217619f227463.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996d32630ac5db005eace9720b9ca69b",
+    "url": "/GDSC-BIET-Website/index.html"
+  },
+  {
+    "revision": "e92f085d3ded82218b65",
+    "url": "/GDSC-BIET-Website/static/css/2.c140bb19.chunk.css"
+  },
+  {
+    "revision": "e74f55c9ed90540c2d2e",
+    "url": "/GDSC-BIET-Website/static/css/main.dcfe398b.chunk.css"
+  },
+  {
+    "revision": "e92f085d3ded82218b65",
+    "url": "/GDSC-BIET-Website/static/js/2.ca9e8dc4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/GDSC-BIET-Website/static/js/2.ca9e8dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e74f55c9ed90540c2d2e",
+    "url": "/GDSC-BIET-Website/static/js/main.15d05c66.chunk.js"
+  },
+  {
+    "revision": "eaea388db9c2b4e4eb9a",
+    "url": "/GDSC-BIET-Website/static/js/runtime-main.0502a3a3.js"
+  },
+  {
+    "revision": "68eb56175095e8fddf0f4de17d795570",
+    "url": "/GDSC-BIET-Website/static/media/Amaresh.68eb5617.jpg"
+  },
+  {
+    "revision": "cc2fbe077f1d29e8c00dca4d0eca06d8",
+    "url": "/GDSC-BIET-Website/static/media/Feedback.cc2fbe07.svg"
+  },
+  {
+    "revision": "3143b6a727d69f06e5dc9ec57242a481",
+    "url": "/GDSC-BIET-Website/static/media/Jahnavi.3143b6a7.jpg"
+  },
+  {
+    "revision": "52778f3b3c543f876a685fad597fbacd",
+    "url": "/GDSC-BIET-Website/static/media/Project-team.52778f3b.svg"
+  },
+  {
+    "revision": "2a3616de4fac94219f1bc1c21d2de000",
+    "url": "/GDSC-BIET-Website/static/media/Rithvik.2a3616de.jpg"
+  },
+  {
+    "revision": "da7ad87ae93f99724e58d6304388926e",
+    "url": "/GDSC-BIET-Website/static/media/Startup-1.da7ad87a.svg"
+  },
+  {
+    "revision": "c46dff878fa825f73396f21b06f8c320",
+    "url": "/GDSC-BIET-Website/static/media/Startup-2.c46dff87.svg"
+  },
+  {
+    "revision": "bcd5c1211b62315f410a8c4abcf9a2cc",
+    "url": "/GDSC-BIET-Website/static/media/Tejaswi.bcd5c121.jpg"
+  },
+  {
+    "revision": "20b011282b1ac64b792b1b265199f695",
+    "url": "/GDSC-BIET-Website/static/media/amazon-web-services.20b01128.svg"
+  },
+  {
+    "revision": "bd954ceeeb42b91899ebe1c3b0d79850",
+    "url": "/GDSC-BIET-Website/static/media/dsc_logo.bd954cee.png"
+  },
+  {
+    "revision": "63a193f6bd8c8d8716468a27b55227c0",
+    "url": "/GDSC-BIET-Website/static/media/firebase.63a193f6.svg"
+  },
+  {
+    "revision": "89e161060662c05666fa7652ecdb0218",
+    "url": "/GDSC-BIET-Website/static/media/flutter.89e16106.svg"
+  },
+  {
+    "revision": "1d1f5e8add975243c7663753515251ea",
+    "url": "/GDSC-BIET-Website/static/media/ideaImage.1d1f5e8a.svg"
+  },
+  {
+    "revision": "f5f6e3202de1a2fe1495f00abb3a80e9",
+    "url": "/GDSC-BIET-Website/static/media/java.f5f6e320.svg"
+  },
+  {
+    "revision": "fa59b833369f41c2586d4892c503508b",
+    "url": "/GDSC-BIET-Website/static/media/javascript.fa59b833.svg"
+  },
+  {
+    "revision": "cdcd8be43c170d3381edd8847223a0c5",
+    "url": "/GDSC-BIET-Website/static/media/kamal.cdcd8be4.jpg"
+  },
+  {
+    "revision": "771c96353ece005ec93b34b8d58d7320",
+    "url": "/GDSC-BIET-Website/static/media/nextjs.771c9635.svg"
+  },
+  {
+    "revision": "a1467103f206de3746c2fb7e149e4f63",
+    "url": "/GDSC-BIET-Website/static/media/pavan.a1467103.jpeg"
+  },
+  {
+    "revision": "0b948ab5dd3f9e988cd9130da860408f",
+    "url": "/GDSC-BIET-Website/static/media/python.0b948ab5.svg"
+  },
+  {
+    "revision": "1d99931b40be38e5ea72ff0767275c26",
+    "url": "/GDSC-BIET-Website/static/media/reactjs.1d99931b.svg"
+  },
+  {
+    "revision": "6ed5dded5c5c625785fb4bb97b1e8bd4",
+    "url": "/GDSC-BIET-Website/static/media/robot.6ed5dded.svg"
+  }
+]);
